07dcee1e19358ac7872ea96da5ac204a2d747479
ushakumarm2005 <144239556+ushakumarm2005@users.noreply.github.com>
2023-09-13 13:56:05 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
-#  Implement a recursive function to calculate the factorial of a given number.
+'''Implement a class called Player that represents a cricket player. The Player class should have a
+method called play() which prints "The player is playing cricket. Derive two classes, Batsman and
+Bowler, from the Player class. Override the play() method in each derived class to print "The batsman
+is batting" and "The bowler is bowling", respectively. Write a program to create objects of both the
+Batsman and Bowler classes and call the play() method for each object.'''
 
 
-def fact_rec(n):
-  if n == 0 or n == 1:
-    return 1
-  else:
-    return n * fact_rec(n - 1)
+# Define the base class Player
+class Player:
+    def play(self):
+        print("The player is playing cricket.")
 
+# Define the derived class Batsman
+class Batsman(Player):
+    def play(self):
+        print("The batsman is batting.")
 
-number = 4
-res = fact_rec(number)
+# Define the derived class Bowler
+class Bowler(Player):
+    def play(self):
+        print("The bowler is bowling.")
 
-print("The factorial of{}is{}".format(number, res))
+# Create objects of Batsman and Bowler classes
+batsman = Batsman()
+bowler = Bowler()
+
+# Call the play() method for each object
+batsman.play()
+bowler.play()
